9e3d9ab61b5759426011f4f99fc846eafdd6f3a6
bjradev <kingrungrothan@gmail.com>
2025-10-29 00:52:33 -0500

    Add test credentials to login page
    Displayed example email and password above the login form to assist users during testing.

--- apps/web-client/src/features/auth/pages/loginPage.tsx
+++ apps/web-client/src/features/auth/pages/loginPage.tsx
@@ -5,6 +5,14 @@ export function LoginPage() {
     <>
       <div className="bg-muted flex w-full h-full flex-col items-center justify-center gap-6 p-6 md:p-10">
         <div className="flex w-full max-w-sm flex-col gap-6">
+          <div className="flex flex-row gap-2 justify-center items-center">
+            <p className="text-sm text-muted-foreground">
+              <span className="font-black">email test:</span> test@testing.com
+            </p>
+            <p className="text-sm text-muted-foreground">
+              <span className="font-black">password test:</span> testing123
+            </p>
+          </div>
           <LoginForm />
         </div>
       </div>
